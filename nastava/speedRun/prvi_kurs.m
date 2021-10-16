48fa17c40840b13f8d5f34799fb8f48a7b4139af
Teodor Biocanin <tekikralj011@gmail.com>
2021-10-16 17:26:21 +0200

    some tasks from cody are here

--- nastava/speedRun/prvi_kurs.m
+++ nastava/speedRun/prvi_kurs.m
@@ -13,3 +13,17 @@
 % plot(jedan_argument) - kada je ovakva situacija, plotuje sve iz y vektora, a za x osu koristi vrednosti od 1 do n
 % gde je n broj elemenata vektora y
 % fft() -> furijeova trans nad nekim vektorom; numel(y) -> broj elemenata vektora y
+% -------------------------------------------------
+% temperature conversion script
+% function C = temp_convert(F)
+
+%     C = ( F - 32 ) ./ 1.8000;
+
+% end
+% -------------------------------------------------
+% function old_name = find_max_age(name, age)
+%     [~, old_name_index] = max(age);
+    
+%     old_name = name(old_name_index);
+% end
+% -------------------------------------------------
