ce527dba721e94a42de51fcd555d04ac8f6e4ff6
Teodor Biocanin <tekikralj011@gmail.com>
2021-11-08 18:05:02 +0100

    initial commit for the seventh week - prog in AE

---
+++ nastava/Sedma nedelja/setma_nedelja.m
@@ -0,0 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % Sedma nedelja nastave - vezbanje pred klk
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% function izlaz1 = sesta_nedelja(ulaz1, ulaz2)
+
+% end
+
+
+% function izlaz1 = sesta_nedelja(ulaz1, ulaz2)
+
+% end
+
+% function izlaz1 = sesta_nedelja(ulaz1, ulaz2)
+
+% end
+
+% function izlaz1 = sesta_nedelja(ulaz1, ulaz2)
+
+% end
